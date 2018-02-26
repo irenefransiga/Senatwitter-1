6733875743c99229b340a5d686735bf947f917cc
Garrett Lahmann <Garrett.Lahmann1@T-Mobile.com>
2018-02-26 12:25:53 -0800

    Add webforms

---
+++ Senatwitter/Senatwitter/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Senatwitter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Senatwitter")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62dd6fc0-4d46-488e-9cf9-3017975ae5ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Senatwitter/Senatwitter/homePage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="Senatwitter.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Senatwitter</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Senatwitter<br />
+            <img src="images/senatweet.jpg" alt="Sample Photo" />
+            <asp:Image ID="Senatweet" runat="server" />
+            <br />
+            <br />
+            <textarea id="TextArea1" cols="20" name="S1" rows="2"></textarea></div>
+        <asp:DataList ID="DataList1" runat="server">
+        </asp:DataList>
+    </form>
+</body>
+</html>
